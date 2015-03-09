53d3a2b881b25fbc9653f29fbbaa88cd5fea3e45
Konstantin Nosov <k.nosov@unistream.com>
2015-03-09 13:23:17 +0300

    Solution adopted for nuget and TC build

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyCompany("InceptumSoft")]
+[assembly: AssemblyProduct("Inceptum.Raft")]
+[assembly: AssemblyCopyright("Copyright © InceptumSoft 2015")]
+[assembly: AssemblyTrademark("Inceptum.Raft")]
+[assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
